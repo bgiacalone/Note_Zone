d9a64cc766997fbc890f5f2cb45d56b4a4b85a77
bgiacalone <bengiacalone@yahoo.com>
2021-03-25 12:04:35 -0400

    Scales page WIP

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   let note_map = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"];
+  let scale_map = [ [2,2,1,2,2,2,1],
+                    [2,1,2,2,1,2,2] ];
   let g = document.getElementById("guitar");
   let k = document.getElementById("keyboard");
   let toggleNoteNamesButton = document.getElementById("toggleNoteNames");
@@ -9,59 +11,56 @@ document.addEventListener("DOMContentLoaded", function(event) {
   let names_shown = false;
   let notes_highlighted = false;
   let scale_highlighted = false;
-  add_class(g, "names_hidden");
-  add_class(k, "names_hidden");
+  k.classList.add("names_hidden");
+  g.classList.add("names_hidden");
   name_notes();
   toggleNoteNamesButton.addEventListener("click", toggle_note_name_click_handler);
   toggleNoteHighlightButton.addEventListener("click", toggle_note_highlight_click_handler);
-  toggleScaleHighlightButton.addEventListener("click", toggle_scale_highlight_click_handler);
+  toggleScaleHighlightButton ? toggleScaleHighlightButton.addEventListener("click", toggle_scale_highlight_click_handler) : console.log("n");
   function toggle_note_highlight_click_handler() {
     if (notes_highlighted) {
-      remove_class(g, "notes_highlighted");
-      remove_class(k, "notes_highlighted");
+      k.classList.remove("notes_highlighted");
+      g.classList.remove("notes_highlighted");
       notes_highlighted = false;
     } else {
-      add_class(g, "notes_highlighted");
-      add_class(k, "notes_highlighted");
+      k.classList.add("notes_highlighted");
+      g.classList.add("notes_highlighted");
       notes_highlighted = true;
     }
   }
-
-
-  let cmaj = [1,3,5,6,8,10,12];
   function toggle_scale_highlight_click_handler() {
+    let scale_dropdown_val = 0;
+    let scale_root_note = 0;
     if (scale_highlighted) {
-      cmaj.forEach((item, i) => {
+      scale_map[scale_dropdown].forEach((item, i) => {
         let asd = document.querySelectorAll("div[data-n='"+item+"']");
         asd.forEach((item, i) => {
-          remove_class(item, "notes_highlighted");
+          item.classList.remove("notes_highlighted");
         });
       });;
       scale_highlighted = false;
     } else {
-      cmaj.forEach((item, i) => {
+      scale_map[scale_dropdown].forEach((item, i) => {
         let asd = document.querySelectorAll("div[data-n='"+item+"']");
         asd.forEach((item, i) => {
-          add_class(item, "notes_highlighted");
+          item.classList.add("notes_highlighted");
         });
       });
       scale_highlighted = true;
     }
   }
-
-
   function toggle_note_name_click_handler() {
     if (names_shown) {
-      remove_class(g, "names_shown");
-      remove_class(k, "names_shown");
-      add_class(g, "names_hidden");
-      add_class(k, "names_hidden");
+      k.classList.remove("names_shown");
+      g.classList.remove("names_shown");
+      k.classList.add("names_hidden");
+      g.classList.add("names_hidden");
       names_shown = false;
     } else {
-      remove_class(g, "names_hidden");
-      remove_class(k, "names_hidden");
-      add_class(g, "names_shown");
-      add_class(k, "names_shown");
+      k.classList.remove("names_hidden");
+      g.classList.remove("names_hidden");
+      k.classList.add("names_shown");
+      g.classList.add("names_shown");
       names_shown = true;
     }
   }
@@ -74,27 +73,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
   }
 });
 
-function has_class(element, klass) {
-  if (element.className && typeof(klass) == "string") {
-    let classes = element.className.split(" ");
-    return classes.includes(klass);
-  } else {
-    return false;
-  }
-}
-function remove_class(element, klass) {
-  let classes = element.className.split(" ");
-  if (classes.includes(klass)) {
-    classes.splice(classes.indexOf(klass), 1);
-    element.className = classes.join(" ");
-  } else {
-    return false;
-  }
-}
-function add_class(element, klass) {
-  let classes = element.className.split(" ");
-  if (!classes.includes(klass)) {
-    classes.push(klass);
-    element.className = classes.join(" ").trim();
-  }
-}
+// function has_class(element, klass) {
+//   if (element.className && typeof(klass) == "string") {
+//     let classes = element.className.split(" ");
+//     return classes.includes(klass);
+//   } else {
+//     return false;
+//   }
+// }
+// function remove_class(element, klass) {
+//   let classes = element.className.split(" ");
+//   if (classes.includes(klass)) {
+//     classes.splice(classes.indexOf(klass), 1);
+//     element.className = classes.join(" ");
+//   } else {
+//     return false;
+//   }
+// }
+// function add_class(element, klass) {
+//   let classes = element.className.split(" ");
+//   if (!classes.includes(klass)) {
+//     classes.push(klass);
+//     element.className = classes.join(" ").trim();
+//   }
+// }
