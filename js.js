f7be3e409fbb18e5cb67e856b95ff21eb7d0c338
Guac Amole <gg@Guacs-MBP.fios-router.home>
2021-03-28 03:23:18 -0400

    partial refactor

--- js.js
+++ js.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-  let note_map = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"];
+  name_notes();
+
   let scale_map = [ [2,2,1,2,2,2,1],
                     [2,1,2,2,1,2,2],
                     [1,2,2,1,2,2,2],
@@ -7,68 +8,42 @@ document.addEventListener("DOMContentLoaded", function(event) {
                     [1,2,2,2,1,2,2],
                     [2,2,2,1,2,2,1],
                     [2,2,1,2,2,1,2] ];
-  let g = document.getElementById("guitar");
-  let k = document.getElementById("keyboard");
+
   let toggleNoteNamesButton = document.getElementById("toggleNoteNames");
   let toggleNoteHighlightButton = document.getElementById("toggleNoteHighlight");
   let toggleScaleHighlightButton = document.getElementById("toggleScaleHighlight");
+
   let selectorScaleTonic = document.getElementById("tonic");
   let selectorScaleInterval = document.getElementById("interval");
+
+  let g = document.getElementById("guitar");
+  let k = document.getElementById("keyboard");
   let keyboard = document.getElementById("keyboard");
   let guitar = document.getElementById("guitar");
-  let duplicateButton = document.getElementById("duplicateButton");
 
-  let templates = document.getElementById("templates");
-  let expansionButtonArray = [ []
-                                  ];
   let keyboardHideButton = document.querySelector("button[name='hideKeyboard']");
   let guitarHideButton = document.querySelector("button[name='hideGuitar']");
   let stored_scale_tonic = document.querySelector("select[id='tonic']").value;
   let stored_scale_interval = document.querySelector("select[id='interval']").value;
-  let notes_named = false;
-  let names_shown = false;
+
   let notes_highlighted = false;
   let scale_highlighted = false;
-  let expansionCount = 0;
-  k.classList.toggle("names_hidden");
-  g.classList.toggle("names_hidden");
-  name_notes();
-  toggleNoteNamesButton.addEventListener("click", toggle_note_name_click_handler);
+
   toggleNoteHighlightButton.addEventListener("click", toggle_note_highlight_click_handler);
   keyboardHideButton.addEventListener("click", keyboard_hide_button_click_handler);
   guitarHideButton.addEventListener("click", guitar_hide_button_click_handler);
-  duplicateButton.addEventListener("click", duplicate_button_click_handler);
+
   toggleScaleHighlightButton ? toggleScaleHighlightButton.addEventListener("click", toggle_scale_highlight_click_handler) : console.log("n");
   selectorScaleTonic ? selectorScaleTonic.addEventListener("change", toggle_scale_highlight_click_handler) : console.log("n");
   selectorScaleInterval ? selectorScaleInterval.addEventListener("change", toggle_scale_highlight_click_handler) : console.log("n");
+
   function keyboard_hide_button_click_handler() {
     keyboard.classList.toggle("hidden");
   }
   function guitar_hide_button_click_handler() {
     guitar.classList.toggle("hidden");
   }
-  function duplicate_button_click_handler() {
-    var sorted_templates = Array.from(document.querySelectorAll("#templates .template"))
-                            .sort(({dataset: {id: a}}, {dataset: {id: b}}) => a.localeCompare(b)); // To reverse it, use `b.localeCompare(a)`.
-    console.log("number of templates: " + `${sorted_templates.length}`);
-    console.log("largest template id: " + `${sorted_templates[sorted_templates.length - 1].dataset.id}`);
 
-    var new_template = sorted_templates[sorted_templates.length - 1].cloneNode(true);
-    new_template.dataset.id = parseInt(new_template.dataset.id) + 1;
-    console.log("new template id: " + `${new_template.dataset.id}`)
-
-    templates.appendChild(new_template);
-    console.log("new template added to dom !!!");
-    // let newClone = template.cloneNode(true);
-    // newClone.id = newClone.id + "" + expansionCount;
-    // newClone.className = newClone.className + "" + expansionCount;
-    // expansionButtonArray[expansionCount][0] = document.querySelector("div." + newClone.className + " button[id='toggleNoteNames']");
-    // console.log(newClone.className);
-    // console.log(document.querySelector("div." + newClone.className + " button[id='toggleNoteNames']"));
-    // console.log(expansionButtonArray[expansionCount][0]);
-    // expansionCount++;
-    // templates.appendChild(newClone);
-  }
   function toggle_note_highlight_click_handler() {
     if (notes_highlighted) {
       k.classList.toggle("notes_highlighted");
@@ -119,29 +94,33 @@ document.addEventListener("DOMContentLoaded", function(event) {
       scale_highlighted = true;
     }
   }
-  function toggle_note_name_click_handler() {
-    if (names_shown) {
-      k.classList.toggle("names_shown");
-      g.classList.toggle("names_shown");
-      k.classList.toggle("names_hidden");
-      g.classList.toggle("names_hidden");
-      names_shown = false;
-    } else {
-      k.classList.toggle("names_hidden");
-      g.classList.toggle("names_hidden");
-      k.classList.toggle("names_shown");
-      g.classList.toggle("names_shown");
-      names_shown = true;
-    }
-  }
-  function name_notes() {
-    let notes = document.getElementsByClassName("n");
-    for (var i = 0; i < notes.length; i++) {
-      notes[i].innerHTML = note_map[notes[i].dataset.n - 1];
-    }
-    notes_named = true;
-  }
 });
-function test() {
-  alert("DING");
+
+function name_notes() {
+  let note_map = ["C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B"];
+  let notes = document.getElementsByClassName("n");
+  for (var i = 0; i < notes.length; i++) {
+    notes[i].innerHTML = note_map[notes[i].dataset.n - 1];
+  }
+}
+
+function toggle_note_names(ding) {
+  // should be added to template initializer , but here will do for now
+  name_notes();
+  var this_template = ding.closest('.template');
+  var this_guitar = this_template.querySelector('.g');
+  var this_keyboard = this_template.querySelector('.k');
+  this_keyboard.classList.toggle("names_shown");
+  this_keyboard.classList.toggle("names_hidden");
+  this_guitar.classList.toggle("names_shown");
+  this_guitar.classList.toggle("names_hidden");
+}
+
+function add_template() {
+  let templates = document.getElementById("templates");
+  var sorted_templates = Array.from(templates.querySelectorAll(".template"))
+                          .sort(({dataset: {id: a}}, {dataset: {id: b}}) => a.localeCompare(b));
+  var new_template = sorted_templates[sorted_templates.length - 1].cloneNode(true);
+  new_template.dataset.id = parseInt(new_template.dataset.id) + 1;
+  templates.appendChild(new_template);
 }
