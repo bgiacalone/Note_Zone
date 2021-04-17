d331d1981e20382f5f44028aaabefe709514b087
Benjamin Giacalone <skeletonsonly@gmail.com>
2021-04-17 15:53:23 -0400

    ding
    ding

--- tempo.js
+++ tempo.js
@@ -23,8 +23,11 @@ function fill_template() {
   let start = document.querySelector("div.controls input[id='start']");
   let end = document.querySelector("div.controls input[id='end']");
   let interval = document.querySelector("div.controls input[id='interval']");
-  let new_row = document.querySelector("div[id='source_template'] tr[id='content']").cloneNode(true);
-  new_row.removeAttribute('id');
+  let row_template = document.querySelector("div[id='source_template'] tr[id='content']").cloneNode(true);
+  row_template.removeAttribute('id');
+  while (row_template.firstChild) {
+    row_template.removeChild(row_template.firstChild);
+  }
   let template_tbody = document.querySelector("div[id='source_template'] tbody");
   let target_table = document.querySelector("div.template table");
   let target = document.querySelector("div.template tbody");
@@ -36,39 +39,52 @@ function fill_template() {
   let table_length = Math.floor((+end.value - +start.value)/bpm_increment);
   let bpm_counter = bpm_start;
   let ms = 60000;
+
   for (var i = 0; i <= table_length; i++) {
+    var new_row = row_template.cloneNode(true);
     let quarter_beat_ms = ms/bpm_counter;
-    new_row.childNodes.forEach((item, i) => {
-      if (i == 1) {
-        item.innerHTML = bpm_counter;
-      } else if (i == 3){
-        item.innerHTML = Number.parseFloat((quarter_beat_ms*4)).toFixed(2);
-      } else if (i == 5) {
-        item.innerHTML = Number.parseFloat((quarter_beat_ms*4*(2/3))).toFixed(2);
-      } else if (i == 7) {
-        item.innerHTML = Number.parseFloat((quarter_beat_ms*2)).toFixed(2);
-      } else if (i == 9) {
-        item.innerHTML = Number.parseFloat((quarter_beat_ms*4*(1/3))).toFixed(2);
-      } else if (i == 11) {
-        item.innerHTML = Number.parseFloat((quarter_beat_ms)).toFixed(2);
-        console.log("bpm: " + bpm_counter + " 1/4ms: " + item.innerHTML + " hz: " + ms_to_hz(item.innerHTML));
-      } else if (i == 13) {
-        item.innerHTML = Number.parseFloat((quarter_beat_ms*4*(1/6))).toFixed(2);
-      } else if (i == 15) {
-        item.innerHTML = Number.parseFloat((quarter_beat_ms/2)).toFixed(2);
-      } else if (i == 17) {
-        item.innerHTML = Number.parseFloat((quarter_beat_ms/3)).toFixed(2);
-      } else if (i == 19) {
-        item.innerHTML = Number.parseFloat((quarter_beat_ms/4)).toFixed(2);
-      } else if (i == 21) {
-        item.innerHTML = Number.parseFloat((quarter_beat_ms/6)).toFixed(2);
-      } else if (i == 23) {
-        item.innerHTML = Number.parseFloat((quarter_beat_ms/8)).toFixed(2);
-      } else if (i == 25) {
-        item.innerHTML = Number.parseFloat((quarter_beat_ms/12)).toFixed(2);
-      }
-    });
+    var ms_array = [
+      Number.parseFloat((quarter_beat_ms*4)).toFixed(2),
+      Number.parseFloat((quarter_beat_ms*4*(2/3))).toFixed(2),
+      Number.parseFloat((quarter_beat_ms*2)).toFixed(2),
+      Number.parseFloat((quarter_beat_ms*4*(1/3))).toFixed(2),
+      Number.parseFloat((quarter_beat_ms)).toFixed(2),
+      Number.parseFloat((quarter_beat_ms*4*(1/6))).toFixed(2),
+      Number.parseFloat((quarter_beat_ms/2)).toFixed(2),
+      Number.parseFloat((quarter_beat_ms/3)).toFixed(2),
+      Number.parseFloat((quarter_beat_ms/4)).toFixed(2),
+      Number.parseFloat((quarter_beat_ms/6)).toFixed(2),
+      Number.parseFloat((quarter_beat_ms/8)).toFixed(2),
+      Number.parseFloat((quarter_beat_ms/12)).toFixed(2)
+    ];
+    var hz_array = [
+      ms_to_hz(Number.parseFloat((quarter_beat_ms*4)).toFixed(2)),
+      ms_to_hz(Number.parseFloat((quarter_beat_ms*4*(2/3))).toFixed(2)),
+      ms_to_hz(Number.parseFloat((quarter_beat_ms*2)).toFixed(2)),
+      ms_to_hz(Number.parseFloat((quarter_beat_ms*4*(1/3))).toFixed(2)),
+      ms_to_hz(Number.parseFloat((quarter_beat_ms)).toFixed(2)),
+      ms_to_hz(Number.parseFloat((quarter_beat_ms*4*(1/6))).toFixed(2)),
+      ms_to_hz(Number.parseFloat((quarter_beat_ms/2)).toFixed(2)),
+      ms_to_hz(Number.parseFloat((quarter_beat_ms/3)).toFixed(2)),
+      ms_to_hz(Number.parseFloat((quarter_beat_ms/4)).toFixed(2)),
+      ms_to_hz(Number.parseFloat((quarter_beat_ms/6)).toFixed(2)),
+      ms_to_hz(Number.parseFloat((quarter_beat_ms/8)).toFixed(2)),
+      ms_to_hz(Number.parseFloat((quarter_beat_ms/12)).toFixed(2))
+    ];
+    var bpm_cell = document.createElement("td");
+    var bpm_cell_bold = document.createElement("strong");
+    bpm_cell_bold.innerHTML = bpm_counter;
+    bpm_cell.appendChild(bpm_cell_bold);
+    new_row.appendChild(bpm_cell);
+    for (var j = 0; j <= 11; j++) {
+      const cell = document.createElement("td");
+      cell.dataset.ms = ms_array[j];
+      cell.dataset.hz = hz_array[j];
+      cell.innerHTML = ms_array[j];
+      cell.classList.add( (j % 2 == 0) ? "twos" : "thirds")
+      new_row.appendChild(cell);
+    }
     bpm_counter = bpm_counter + bpm_increment;
-    target.appendChild(new_row.cloneNode(true));
+    target.appendChild(new_row);
   }
 }
