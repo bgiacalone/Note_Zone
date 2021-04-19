90272038fe25bc8168722cbb9ad96c23cf54b2f1
Guac Amole <gg@Guacs-MBP.fios-router.home>
2021-04-19 17:52:05 -0400

    ringding:

--- scripts/windoz.js
+++ scripts/windoz.js
@@ -1,4 +1,15 @@
 function remove(el) {
   var parent = el.closest('.img_item');
   parent.remove();
+  console.log("delete");
+}
+function maximize(el) {
+  var parent = el.closest('.img_item');
+  parent.classList.add('max');
+  console.log("max");
+}
+function min(el) {
+  var parent = el.closest('.img_item');
+  parent.classList.remove('max');
+  console.log("min");
 }
